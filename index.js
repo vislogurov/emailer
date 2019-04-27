2b47fc8dde40f5b2d63b1e8536de38a97621fe21
Maxon Vislogurov <vislogurov@yandex.ru>
2019-04-27 12:33:33 +0300

    добавил eslint

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
 const app = express();
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 // немедленно вызываем authRoutes
 require('./routes/authRoutes')(app);
