db6f044eb3a41979a48457ec24baeadda98030ad
Maxon Vislogurov <vislogurov@yandex.ru>
2019-04-27 11:28:41 +0300

    рефактор папок

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
-
 const app = express();
+require('./services/passport');
 
-passport.use(new GoogleStrategy(
-  {
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    // TODO: протестить возможность смены урла
-    callbackURL: '/auth/google/callback'
-  },
-  (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile);
-    }
-  )
-);
-
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}))
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+// немедленно вызываем authRoutes
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
