b535a24b1619570f33ccd7c61a444cdb356ebc82
Maxon Vislogurov <vislogurov@yandex.ru>
2019-05-03 13:49:40 +0300

    добавил роут платежей

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const passport = require('passport');
 const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
-
-const app = express();
 require('./models/User');
 require('./services/passport');
 
-app.use(cookieSession({
+mongoose.connect(keys.mongoURI);
 
-  // Cookie Options
-  maxAge: 30 * 24 * 60 * 60 * 1000,
-  keys: [keys.cookieKey],
-}));
+const app = express();
 
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  }),
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-mongoose.connect(keys.mongoURI);
-
-// немедленно вызываем authRoutes
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
